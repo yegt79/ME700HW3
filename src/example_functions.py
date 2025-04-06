0056b1b7ebe0374412073ac0919566b944a5d2b5
yegt79 <yegt@bu.edu>
2025-04-06 09:37:38 -0500

    Delete src directory

--- src/example_functions.py
+++
@@ -1,8 +0,0 @@
-def add_x_y(x: float, y: float) -> float:
-    """Given two float values. Will return their sum."""
-    val = x + y
-    return val
-
-
-def hello_world():
-    return "hello world!"
